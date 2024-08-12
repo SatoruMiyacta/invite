433f5e64080685df3695ab23e89135f8bb8a40fa
miyashita <s.miyashita1996@gmail.com>
2024-08-12 17:43:15 +0900

    各画像挿入2

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,15 +1,35 @@
 'use client'
 
-import Typography from '@mui/material/Typography'
 import styles from './page.module.css'
 import Image from 'next/image';
-import { useEffect } from 'react';
 
 export default function Home() {
-
   return (
     <>
       <main className={styles.main}>
+        <section className={styles.fullscreen}>
+          <Image src="/ho2.svg" alt="home2" className={styles.responsiveImage} layout="responsive" width={1920} height={1080} />
+        </section>
+        <section className={styles.fullscreen}>
+          <Image src="/ho3.svg" alt="home3" className={styles.responsiveImage} layout="responsive" width={1920} height={1080} />
+        </section>
+        <section className={styles.fullscreen}>
+          <Image src="/ho4.svg" alt="home4" className={styles.responsiveImage} layout="responsive" width={1920} height={1080} />
+        </section>
+        <section className={styles.fullscreen}>
+          <Image src="/ho5.svg" alt="home5" className={styles.responsiveImage} layout="responsive" width={1920} height={1080} />
+        </section>
+        <section className={styles.fullscreen}>
+          <Image src="/ho6.svg" alt="home6" className={styles.responsiveImage} layout="responsive" width={1920} height={1080} />
+        </section>
+        <section className={styles.fullscreen}>
+          <Image src="/ho7.svg" alt="home7" className={styles.responsiveImage} layout="responsive" width={1920} height={1080} />
+        </section>
+      </main>
+    </>
+  )
+}
+
         {/* <section className={styles.fullscreen} style={{ position: 'relative', height: '33.33vh', width: '100vw', overflow: 'hidden' }}>
           <Image src="/home_top.svg" alt="top" fill style={{ objectFit: 'contain', objectPosition: 'top', width: '100%', height: '100%' }} />  
         </section>
@@ -19,25 +39,6 @@ export default function Home() {
         <section className={styles.fullscreen} style={{ position: 'relative', height: '33.33vh', width: '100vw', overflow: 'hidden' }}>
           <Image src="/home_under.svg" alt="bottom" fill style={{ objectFit: 'contain', objectPosition: 'bottom', width: '100%', height: '100%' }} />
         </section> */}
-        <section className={styles.fullscreen}>
-          <Image src="/home1.jpg" alt="home1" fill style={{ objectFit: 'contain', width: '100%', height: '100%' }} />
-        </section>
-        <section className={styles.fullscreen}>
-          <Image src="/home2.jpg" alt="home2" fill style={{ objectFit: 'contain', width: '100%', height: '100%' }} />
-        </section>
-        <section className={styles.fullscreen}>
-          <Image src="/home3.jpg" alt="home3" fill style={{ objectFit: 'contain', width: '100%', height: '100%' }} />
-        </section>
-        <section className={styles.fullscreen}>
-          <Image src="/home4.jpg" alt="home4" fill style={{ objectFit: 'contain', width: '100%', height: '100%' }} />
-        </section>
-        <section className={styles.fullscreen}>
-          <Image src="/home5.jpg" alt="home5" fill style={{ objectFit: 'contain', width: '100%', height: '100%' }} />
-        </section>
-        <section className={styles.fullscreen}>
-          <Image src="/home7.jpg" alt="home7" fill style={{ objectFit: 'contain', width: '100%', height: '100%' }} />
-        </section>
-      </main>
-    </>
-  )
-}
+        {/* <section className={styles.fullscreen}>
+          <Image src="/ho1.svg" alt="home1" fill style={{ objectFit: 'contain', width: '100%', height: '100%' }} />
+        </section> */}
