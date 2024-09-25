8ba461b815537ab58ca7036e0484f2a0588ceecd
miyashita <s.miyashita1996@gmail.com>
2024-09-25 23:23:23 +0900

    遅延追加

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -45,156 +45,131 @@ export default function Home() {
     }
   }, [loading])
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false)
-    }, 500)
-
-    return () => clearTimeout(timer)
-  }, [])
-
   return (
     <>
       <main className={styles.main}>
-        {loading ? (
-          <div className={styles.center}>
+        {loading && (
+          <div className={styles.spinner}>
             <CircularProgress />
           </div>
-        ) : (
-          <>
-            <div className={styles.imageContainer}>
-              <Image
-                src='/2.svg'
-                className={styles.image}
-                alt={''}
-                width={100}
-                height={100}
-                priority={true}
-              />
-            </div>
-            <div className={styles.imageContainer}>
-              <Image
-                src='/3.svg'
-                className={styles.image}
-                alt={''}
-                width={100}
-                height={100}
-                loading='lazy'
-              />
-            </div>
-            <div className={styles.imageContainer}>
-              <Image
-                src='/4.svg'
-                className={styles.image}
-                alt={''}
-                width={100}
-                height={100}
-              />
-            </div>
-            <div className={styles.imageContainer}>
-              <Image
-                src='/5.svg'
-                className={styles.image}
-                alt={''}
-                width={100}
-                height={100}
-                loading='lazy'
-              />
-            </div>
-            <div className={styles.imageContainer}>
-              <Image
-                src='/6.svg'
-                className={styles.image}
-                alt={''}
-                width={100}
-                height={100}
-                loading='lazy'
-              />
-            </div>
-            <div className={styles.imageContainer}>
-              <Image
-                src='/7.svg'
-                className={styles.image}
-                alt={''}
-                width={100}
-                height={100}
-                loading='lazy'
-              />
-            </div>
-            <div className={styles.imageContainer} style={{ position: 'relative' }}>
-              <Image
-                src='/8.svg'
-                className={styles.image}
-                alt={''}
-                width={100}
-                height={100}
-                loading='lazy'
-              />
-              <div className={styles.linkContainer}>
-                <Link
-                  target='_blank'
-                  href='https://maps.app.goo.gl/Ho8ksuGVb414jxy86'
-                  passHref
-                >
-                  <Button
-                    variant='contained'
-                    size='large'
-                    color='success'
-                    endIcon={<ArrowForwardIcon />}
-                  >
-                    Googleマップを表示
-                  </Button>
-                </Link>
+        )}
+        <div className={styles.imageContainer}>
+          <Image
+            src='/2.svg'
+            className={styles.image}
+            alt={''}
+            width={100}
+            height={100}
+            priority={true}
+          />
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src='/3.svg'
+            className={styles.image}
+            alt={''}
+            width={100}
+            height={100}
+          />
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src='/4.svg'
+            className={styles.image}
+            alt={''}
+            width={100}
+            height={100}
+            onLoad={() => setLoading(false)}
+          />
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src='/5.svg'
+            className={styles.image}
+            alt={''}
+            width={100}
+            height={100}
+          />
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src='/6.svg'
+            className={styles.image}
+            alt={''}
+            width={100}
+            height={100}
+          />
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src='/7.svg'
+            className={styles.image}
+            alt={''}
+            width={100}
+            height={100}
+          />
+        </div>
+        <div className={styles.imageContainer} style={{ position: 'relative' }}>
+          <Image
+            src='/8.svg'
+            className={styles.image}
+            alt={''}
+            width={100}
+            height={100}
+          />
+          <div className={styles.linkContainer}>
+            <Link
+              target='_blank'
+              href='https://maps.app.goo.gl/Ho8ksuGVb414jxy86'
+              passHref
+            >
+              <Button
+                variant='contained'
+                size='large'
+                color='success'
+                endIcon={<ArrowForwardIcon />}
+              >
+                Googleマップを表示
+              </Button>
+            </Link>
+          </div>
+        </div>
+        <div className={styles.imageContainer} style={{ position: 'relative' }}>
+          <Image
+            src='/9.svg'
+            className={styles.image}
+            alt={''}
+            width={100}
+            height={100}
+          />
+          <h1 className={`${Font.className} ${styles.head}`} style={{ fontSize: '4rem' }}>
+            {oneTheDay ?? 'Count Down'}
+          </h1>
+          {!oneTheDay && (
+            <div className={`${Font.className} ${styles.countdown}`}>
+              <div className={`${styles.countdownItem}`} style={{ fontSize: '32px' }}>
+                <span className={styles.countdownValue} style={{ fontSize: '4rem' }}>
+                  {timeLeft.split(' ')[0]}
+                </span>
+                <span className={styles.countdownLabel}>DAYS</span>
+              </div>
+              <div className={styles.flex}>
+                <div className={`${styles.countdownItem}`}>
+                  <span className={styles.countdownValue}>{timeLeft.split(' ')[1]}</span>
+                  <span className={styles.countdownLabel}>HOURS</span>
+                </div>
+                <div className={`${styles.countdownItem}`}>
+                  <span className={styles.countdownValue}>{timeLeft.split(' ')[2]}</span>
+                  <span className={styles.countdownLabel}>MINUTES</span>
+                </div>
+                <div className={`${styles.countdownItem}`}>
+                  <span className={styles.countdownValue}>{timeLeft.split(' ')[3]}</span>
+                  <span className={styles.countdownLabel}>SECONDS</span>
+                </div>
               </div>
             </div>
-            <div className={styles.imageContainer} style={{ position: 'relative' }}>
-              <Image
-                src='/9.svg'
-                className={styles.image}
-                alt={''}
-                width={100}
-                height={100}
-                loading='lazy'
-              />
-              <h1
-                className={`${Font.className} ${styles.head}`}
-                style={{ fontSize: '4rem' }}
-              >
-                {oneTheDay ?? 'Count Down'}
-              </h1>
-              {!oneTheDay && (
-                <div className={`${Font.className} ${styles.countdown}`}>
-                  <div className={`${styles.countdownItem}`} style={{ fontSize: '32px' }}>
-                    <span className={styles.countdownValue} style={{ fontSize: '4rem' }}>
-                      {timeLeft.split(' ')[0]}
-                    </span>
-                    <span className={styles.countdownLabel}>DAYS</span>
-                  </div>
-                  <div className={styles.flex}>
-                    <div className={`${styles.countdownItem}`}>
-                      <span className={styles.countdownValue}>
-                        {timeLeft.split(' ')[1]}
-                      </span>
-                      <span className={styles.countdownLabel}>HOURS</span>
-                    </div>
-                    <div className={`${styles.countdownItem}`}>
-                      <span className={styles.countdownValue}>
-                        {timeLeft.split(' ')[2]}
-                      </span>
-                      <span className={styles.countdownLabel}>MINUTES</span>
-                    </div>
-                    <div className={`${styles.countdownItem}`}>
-                      <span className={styles.countdownValue}>
-                        {timeLeft.split(' ')[3]}
-                      </span>
-                      <span className={styles.countdownLabel}>SECONDS</span>
-                    </div>
-                  </div>
-                </div>
-              )}
-            </div>
-          </>
-        )}
+          )}
+        </div>
       </main>
     </>
   )
